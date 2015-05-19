8fafc7c90ae3a6471ab4da747a7a2b8a71c86471
Fabien Antoine <fab.antoine@gmail.com>
2015-05-19 09:15:29 +0200

    Fixed Symfony completion installer

--- scripts/symfony/completion.sh
+++ scripts/symfony/completion.sh
@@ -5,7 +5,8 @@ completion='/home/vagrant/symfony2-autocomplete.bash'
 if [ ! -f $completion ]; then
     echo 'Installing Symfony console completion'
     wget -O $completion https://raw.githubusercontent.com/KnpLabs/symfony2-autocomplete/master/symfony2-autocomplete.bash >/dev/null 2>&1
-    chmod a+x $completion
+    chmod a+x "$completion"
+    chown vagrant:vagrant "$completion"
 
     echo '# Symfony2 completion' >> /home/vagrant/.bashrc
     echo "source $completion" >> /home/vagrant/.bashrc
