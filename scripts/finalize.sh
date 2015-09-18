833f81f0c45d724812d2172e510f28e3d97f46e2
Fabien Antoine <fab.antoine@gmail.com>
2015-09-18 14:48:27 +0200

    Fixed finalize script

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -2,8 +2,8 @@
 
 # Installing login 'cd'
 cdVagrant='cd /vagrant'
-if ! grep -q $cdVagrant /home/vagrant/.bashrc ; then
+if ! grep -q "$cdVagrant" /home/vagrant/.bashrc ; then
     echo 'Setup home directory'
     echo '# Go to vagrant' >> /home/vagrant/.bashrc
-    echo $cdVagrant >> /home/vagrant/.bashrc
+    echo "$cdVagrant" >> /home/vagrant/.bashrc
 fi
